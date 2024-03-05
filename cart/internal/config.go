b69c5d765bedbb138bbbfd0e5c36c2acb7180a1e
kharin <Aleksandr.Kharin@leomax.ru>
2024-03-05 12:13:46 +0300

    [GOHW-1]: Added product service timeout to config

--- cart/internal/config.go
+++ cart/internal/config.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"github.com/sethvargo/go-envconfig"
 	"log"
+	"time"
 )
 
 type Config struct {
-	CartHost                  string `env:"CART_HOST, default=0.0.0.0:8080"`
-	ProductServiceUrl         string `env:"PRODUCT_SERVER_URL, default=http://route256.pavl.uk:8080"`
-	ProductServiceToken       string `env:"PRODUCT_SERVER_TOKEN, default=testtoken"`
-	ProductServiceRetryStatus []int  `env:"PRODUCT_SERVER_RETRY_STATUS, default=420, 429"`
-	ProductServiceRetryCount  int    `env:"PRODUCT_SERVER_RETRY_COUNT, default=3"`
+	CartHost                  string        `env:"CART_HOST, default=0.0.0.0:8080"`
+	ProductServiceUrl         string        `env:"PRODUCT_SERVER_URL, default=http://route256.pavl.uk:8080"`
+	ProductServiceToken       string        `env:"PRODUCT_SERVER_TOKEN, default=testtoken"`
+	ProductServiceRetryStatus []int         `env:"PRODUCT_SERVER_RETRY_STATUS, default=420, 429"`
+	ProductServiceRetryCount  int           `env:"PRODUCT_SERVER_RETRY_COUNT, default=3"`
+	ProductServiceTimeout     time.Duration `env:"PRODUCT_SERVER_TIMEOUT, default=5s"`
 }
 
 func GetConfig() *Config {
