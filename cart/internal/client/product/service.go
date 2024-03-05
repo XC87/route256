684f78819bbfa74bab79fe82a1e730ba75edbd45
kharin <Aleksandr.Kharin@leomax.ru>
2024-03-05 12:13:46 +0300

    [GOHW-1]: Added product service timeout to config

--- cart/internal/client/product/service.go
+++ cart/internal/client/product/service.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"route256.ozon.ru/project/cart/internal"
-	"time"
 )
 
 const (
@@ -31,7 +30,7 @@ func NewProductService(config *internal.Config) *ProductService {
 			RetryCodes: config.ProductServiceRetryStatus,
 			MaxRetries: config.ProductServiceRetryCount,
 		},
-		Timeout: 5 * time.Second,
+		Timeout: config.ProductServiceTimeout,
 	}
 	return &ProductService{client: client, host: config.ProductServiceUrl, token: config.ProductServiceToken}
 }
