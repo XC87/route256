09c1e2e0d1c864ed92760c4058f9819a4d76a6ce
kharin <Aleksandr.Kharin@leomax.ru>
2024-03-22 09:18:23 +0300

    [GOHW-3]: Small fixes

--- cart/internal/config/cart.go
+++ cart/internal/config/cart.go
@@ -8,7 +8,7 @@ import (
 
 type Config struct {
 	CartHost                  string        `env:"CART_HOST, default=0.0.0.0:8080"`
-	LomsGrpcHost              string        `env:"LOMS_HOST, default=0.0.0.0:50051"`
+	LomsGrpcHost              string        `env:"LOMS_GRPC_HOST, default=0.0.0.0:50051"`
 	ProductServiceUrl         string        `env:"PRODUCT_SERVER_URL, default=http://route256.pavl.uk:8080"`
 	ProductServiceToken       string        `env:"PRODUCT_SERVER_TOKEN, default=testtoken"`
 	ProductServiceRetryStatus []int         `env:"PRODUCT_SERVER_RETRY_STATUS, default=420, 429"`
